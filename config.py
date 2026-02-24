9e1c67d27776dda5c7e69c6c8fc06154797fb841
safarovd272-eng <safarovd272@gmail.com>
2026-02-24 22:24:52 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 import os
 
-# .env fayldan yoki to'g'ridan-to'g'ri yozing
-BOT_TOKEN = os.getenv("8587190645:AAElrROU42jTdiEts84raSm2t9qIlHfPSKo", "8587190645:AAE0q_5DSnRpHcyg8WE248l29gSKZ1CuDe8")
+BOT_TOKEN = "8587190645:AAE0q_5DSnRpHcyg8WE248l29g5KZ1CuDe8"
